40f9230452cb59e4fa13bd1465ce2ac9f911fc0b
Jordan Bryan <jbryan382@gmail.com>
2019-03-13 21:40:01 -0400

    Set Up program successfully

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GuessingGame
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Think of a number between 1-100.");
+    }
+  }
+}
