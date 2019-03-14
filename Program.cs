f91813088c1c11ce88128b3b4be0aacc4203bdee
Jordan Bryan <jbryan382@gmail.com>
2019-03-13 23:46:37 -0400

    Got Logic up and running needs a tweak or two

--- Program.cs
+++ Program.cs
@@ -6,7 +6,37 @@ namespace GuessingGame
   {
     static void Main(string[] args)
     {
+      var Guessing = true;
+      var count = 0;
+      var Guess = 100;
+      //   var Max = 100;
+      //   var Min = 0;
       Console.WriteLine("Think of a number between 1-100.");
+      while (Guessing == true)
+      {
+        count++;
+        Console.WriteLine($"Is your number {Guess}");
+        var input = Console.ReadLine();
+        if (input == "yes")
+        {
+          Guessing = false;
+        }
+        else
+        {
+          Console.WriteLine("higher or lower?");
+          var HOrL = Console.ReadLine();
+          if (HOrL == "higher")
+          {
+            Guess = Guess + Guess / 2;
+          }
+          else
+          {
+            Guess = Guess / 2;
+          }
+        }
+      }
+      Console.WriteLine($"Hey, I guess a total of {count} times and your number was {Guess}");
+
     }
   }
 }
